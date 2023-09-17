0e09f6f8def5005e0e83d79c0dc54ec940aa7140
Ridwan <ridwanmasykur05@gmail.com>
2023-09-17 15:40:12 +0700

    updatetujuhbelas

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,6 @@ window.onbeforeunload = () => {
 
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -30,6 +29,6 @@ function scrollFunction() {
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
